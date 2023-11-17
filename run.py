3f810997c087882b12b36fb39baacac5dc55db92
CharlottaG <>
2023-11-17 14:22:25 +0000

    Add game instructions

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def instructions():
+    """Print instructions to user on how to play the game."""
+    print("Can you find the code guarding the secret message?")
+    print("It is a 4 digit code with numbers between 1 and 6, no duplications.\n")
+    print("You have 10 tries before the message will self-desctruct.\n")
+    print("   ! indicates one correct number, placed correct.\n")
+    print("   * indicates one correct number, placed wrong.\n")
+    print("Good Luck and Hurry Up!\n")
+    print("__________________________________________________\n")
+
+instructions()
