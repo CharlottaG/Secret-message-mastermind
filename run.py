b434347654880c6d92475df91360a10a55e15f5a
CharlottaG <>
2023-11-17 15:18:34 +0000

    Add function to ask user to enter a guess and validate it for 4 digits, no other characters, no duplications

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ def instructions():
     """Print instructions to user on how to play the game."""
     print("Can you find the code guarding the secret message?")
     print("It is a 4 digit code with numbers between 1 and 6, no duplications.\n")
-    print("You have 10 tries before the message will self-desctruct.\n")
+    print("You have 10 tries before the message will self-destruct.\n")
     print("   ! indicates one correct number, placed correct.\n")
     print("   * indicates one correct number, placed wrong.\n")
     print("Good Luck and Hurry Up!\n")
@@ -20,4 +20,41 @@ def get_username():
         else:
             print("You can't keep it a secret! Please enter your name:")
 
-instructions()
+def guess():
+    """Asks user to enter 4 digits, validates for numbers and no duplication."""
+
+    while True:
+        user_input = input("Please enter a 4 digit code: \n")
+        # Validates for not being empty
+        if user_input == "":
+            print("No, no, no! You can't leave this empty!\n")
+        # Validates digits and no other characters
+        elif not user_input.isdigit():
+            print("No, no, no! Digits only!\n")
+        # Validates 4 digits
+        elif len(user_input) != 4:
+            print("No, no, no! 4 digits please!\n")
+        # Validates duplication
+        elif has_duplication(user_input) == True:
+            print("No, no, no! No duplications!\n")   
+        # Prints guess and increments number for each new guess
+        else:
+            print(f"Guess 1: {user_input}\n")
+            break
+
+def has_duplication(code):
+    """Checks for duplication in code"""
+    entered_numbers = set()
+    for digit in code:
+        if digit in entered_numbers:
+            return True
+        entered_numbers.add(digit)
+    return False
+
+
+def main():
+    instructions()
+    get_username()
+    guess()
+
+main()
