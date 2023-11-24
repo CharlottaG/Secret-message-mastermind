94f7601747984919f1f2e4de1d865c6a231ccc88
CharlottaG <>
2023-11-24 12:19:21 +0000

    Add credentials to import secret message from google sheet

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
 import random
-#import gspread
-#from google.oauth2.service_account import Credentials
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('MasterMindMessages')
+
+messages = SHEET.worksheet('messages')
+
+secret_message = messages.get_all_values()
+
+print(secret_message)
 
 random_code = ""
 user_input = ""
